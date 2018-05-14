ff6668a0ab48302e59a6066d357fcb8812dd3746
couetilc <couetilc@gmail.com>
2018-05-14 20:32:17 +0000

    Works. Updated to included previews and modified to process events from S3 rather than cloudtrail

--- shaveImage.js
+++ shaveImage.js
@@ -2,36 +2,55 @@ const aws = require('aws-sdk');
 const s3 = new aws.S3();
 const sharp = require('sharp');
 
-exports.handler = event => {
-	const bucket = event.detail.requestParameters.bucketName;
-	const key = event.detail.requestParameters.key;
+exports.handler = (event, context) => {
+	console.log(JSON.stringify(event));
+	console.log(JSON.stringify(context));
 
-	console.log(event);
-	console.log([bucket, key]);
+	const bucket = decodeURI(event.Records[0].s3.bucket.name);
+	const key = decodeURI(event.Records[0].s3.object.key);
+
+	const modifications = {
+		"thumbnail": image => sharp(image)
+			.jpeg({
+				quality: 85,
+				progress: true,
+				chromeSubsampling: '4:2:2',
+				force: true
+			})
+			.resize(200, 160)
+			.crop('center')
+			.toFormat('jpeg')
+			.toBuffer(),
+		"preview": image => sharp(image)
+			.jpeg({
+				quality: 85,
+				progress: true,
+				chromeSubsampling: '4:2:2',
+				force: true
+			})
+			.resize(200, 160)
+			.min()
+			.toBuffer()
+	};
 
 	const destination = 'optimized-portfolio';
-	exports.makeThumbnail(bucket, destination, key);
+	for (let mod in modifications) {
+		if (modifications.hasOwnProperty(mod)) {
+			exports.modify(mod, modifications[mod], bucket, destination, key);
+		}
+	}
 };
 
-exports.makeThumbnail = (source, destination, key) => {
-	const getSharpThumbnail = image => sharp(image)
-		.jpeg({
-			quality: 85,
-			progress: true,
-			chromeSubsampling: '4:2:2',
-			force: true
-		})
-		.resize(200, 160)
-		.crop('center')
-		.toFormat('jpeg');
-
+exports.modify = (type, modifyImage, source, destination, key) => {
 	s3.getObject({Bucket: source, Key: key}).promise()
-		.then(object => getSharpThumbnail(object.Body).toBuffer())
-		.then(buffer => s3.putObject({
-			Body: buffer,
-			Bucket: destination,
-			Key: 'thumbnail/' + key
-		}).promise())
-		.then(msg => console.log(['success', msg]))
+		.then(object => modifyImage(object.Body))
+		.then(buffer => {
+			return s3.putObject({
+				Body: buffer,
+				Bucket: destination,
+				Key: [type, key].join('/')
+			}).promise();
+		})
+		.then(msg => console.log(msg))
 		.catch(err => console.log(err));
 };
