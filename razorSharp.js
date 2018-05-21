6751c4f8ced29baebe69cc0aacb260268d8764bd
couetilc <couetilc@gmail.com>
2018-05-21 23:39:43 +0000

    Modifying architecture to reflect using one lambda function

---
+++ razorSharp.js
@@ -0,0 +1,59 @@
+const sharp = require('sharp');
+
+exports.shaves = [
+	{
+		"format": "preview",
+		"cut": image => sharp(image)
+			.jpeg({
+				quality: 85,
+				progress: true,
+				chromeSubsampling: '4:2:2',
+				force: true
+			})
+			.resize(200, 160)
+			.min()
+			.toFormat('jpeg')
+			.toBuffer()
+	},
+	{
+		"format": "original",
+		"cut": image => sharp(image)
+			.jpeg({
+				quality: 100,
+				progress: true,
+				chromeSubsampling: '4:4:4',
+				force: true
+			})
+			.toFormat('jpeg')
+			.toBuffer();
+	},
+	{
+		"format": "thumbnail",
+		"cut": image => sharp(image)
+			.jpeg({
+				quality: 85,
+				progress: true,
+				chromeSubsampling: '4:2:2',
+				force: true
+			})
+			.resize(200, 160)
+			.crop('center')
+			.toFormat('jpeg')
+			.toBuffer();
+	},
+	{
+		"format": "background",
+		"cut": image => sharp(image)
+			.jpeg({
+				quality: 95,
+				progress: true,
+				chromeSubsampling: '4:2:2',
+				force: true
+			})
+			.resize(1920, 1080)
+			.withoutEnlargement()
+			.min()
+			.toFormat('jpeg')
+			.toBuffer();
+	}
+];
---
+++ shaveImage.js
@@ -0,0 +1,36 @@
+const aws = require('aws-sdk');
+const s3 = new aws.S3();
+const db = new aws.DynamoDB();
+const sharp = require('sharp');
+const razor = require('./razorSharp.js');
+
+exports.handler = async (event, context) => {
+	console.log(JSON.stringify(event));
+	console.log(JSON.stringify(context));
+
+	const key = decodeURIComponent(event.Records[0].s3.object.key);
+	const source_bucket = decodeURIComponent(event.Records[0].s3.bucket.name);
+	const event_time = decodeURIComponent(event.Records[0].eventTime);
+	const date_added = event_time.substring(0, event_time.lastIndexOf('.')); //keep seconds
+	const source_filename = key.replace(/\+/g, ' ');
+
+	/* Source Filename Format: "[RANK]<TITLE>(<CATEGORY>, <YEAR>)" */
+	const parseMetadata = /([0-9]*)[ \t]*(.*)[\(](.*),(.*?)[\)]*\.(?=[^.]*$)/g;
+	const fields = parseMetadata.exec(source_filename);
+	const rank = !fields[1] || fields[1] === '' ? '0' : fields[1];
+	const title = fields[2].trim();
+	const category = fields[3].trim();
+	const year_created = fields[4].trim();
+	const artist = 'Olga Gorman';
+
+
+	let promises = [];
+	
+	razor.shaves.forEach(shave => {
+		const destination_filename = [title, 'by', artist, year_created].join(' ');
+		const path = [form, category, destination_filename].join('/');
+		const destination_bucket = 'optimized-portfolio';
+		const s3url = encodeURI('https://s3.amazonaws.com/' + destination_bucket + '/' + path);
+
+	});
+};
