34a5fd721fffc16cb8daefb1567f145c33acb449
couetilc <couetilc@gmail.com>
2018-06-10 18:53:09 +0000

    Experimenting with rendering template files.  Working through handling buffers in AWS Lambda environment

--- touchupHTML.js
+++ touchupHTML.js
@@ -1,9 +1,22 @@
 const aws = require('aws-sdk');
-const s3 = aws.S3();
-const db = aws.DynamoDB();
+const s3 = new aws.S3({apiVersion: '2006-03-01'});
+const db = new aws.DynamoDB({apiVersion: '2012-08-10'});
+const njk = require('nunjucks');
 
-exports.handler = event => {
-	console.log(JSON.stringify(event));
+exports.handler = async event => {
+	//console.log(JSON.stringify(event));
+
+	return s3.getObject({
+		Bucket: "portfolio-originals",
+		Key: "templates/" + "skeleton.njk"
+	}).promise()
+	.then(response => {
+		const buf = response.Body.data;
+		//console.log(JSON.stringify(response));
+		return console.log(Buffer.from(buf)
+			.toString());
+	})
+	.catch(err => console.error(err));
 
 	/* Pull all database information */
 
