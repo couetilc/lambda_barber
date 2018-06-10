1b9fefa9a64eaf9dbdd0cd50d656f6cdc2f44902
couetilc <couetilc@gmail.com>
2018-06-10 02:17:56 +0000

    created handler for the portfolio-originals S3 bucket to invoke specific Lambda functions. Update shaveImage function to reflect changed invocation structure.

---
+++ handlePortfolioPut.js
@@ -0,0 +1,27 @@
+const aws = require('aws-sdk');
+const s3 = new aws.S3({apiVersion: '2006-03-01'});
+const lambda = new aws.Lambda({apiVersion: '2015-03-31'});
+
+exports.handler = event => {
+	console.log(JSON.stringify(event));
+
+	const record = event.Records[0];
+	/* object of size zero indicates a folder */
+	if (record.s3.object.size === 0) return;
+
+	const key = decodeURIComponent(record.s3.object.key);
+	const source_bucket = decodeURIComponent(record.s3.bucket.name);
+	const event_time = decodeURIComponent(record.eventTime);
+
+	if (key.indexOf("portfolio/") === 0) {
+		lambda.invoke({
+			FunctionName: "shaveImage",
+			InvocationType: "Event",
+			Payload: JSON.stringify({
+				"key": key.replace("portfolio/", ""),
+				"source_bucket": source_bucket,
+				"time": event_time
+			})
+		});
+	}
+};
--- shaveImage.js
+++ shaveImage.js
@@ -1,20 +1,16 @@
 const aws = require('aws-sdk');
-const s3 = new aws.S3();
-const db = new aws.DynamoDB();
+const s3 = new aws.S3({apiVersion: '2006-03-01'});
+const db = new aws.DynamoDB({apiVersion: '2012-08-10'});
 const sharp = require('sharp');
 const razor = require('./razorSharp.js');
 
 exports.handler = event => {
 	console.log(JSON.stringify(event));
 
-    /* A put event for an object of size 0 indicates a folder has been made */
-    if (event.Records[0].s3.object.size === 0) return;
-
-	const key = decodeURIComponent(event.Records[0].s3.object.key);
-	const source_bucket = decodeURIComponent(event.Records[0].s3.bucket.name);
-	const event_time = decodeURIComponent(event.Records[0].eventTime);
-	const date_added = event_time.substring(0, event_time.lastIndexOf('.')); //keep seconds
-	const source_filename = key.replace(/\+/g, ' ');
+	const source_filename = event.key.replace(/\+/g, ' ');
+	const source_bucket = event.source_bucket;
+	const date_added = event.time
+		.substring(0, event.time.lastIndexOf('.')); //keep seconds
 
 	/* Source Filename Format: "[RANK]<TITLE>(<CATEGORY>, <YEAR>)" */
 	const parseMetadata = /([0-9]*)[ \t]*(.*)[\(](.*),(.*?)[\)]*\.(?=[^.]*$)/g;
@@ -29,8 +25,10 @@ exports.handler = event => {
 	let promises = [];
 	
 	razor.shaves.forEach(shave => {
-		const filename = [title, 'by', artist, year_created].join(' ') + '.jpg';
-		const path = [shave.style, category, filename].join('/');
+		const filename = [title, 'by', artist, year_created]
+			.join(' ') + '.jpg';
+		const path = ["media", shave.style, category, filename]
+			.join('/');
 		const primary_key = source_filename + ' ' + shave.style;
 		const s3url = encodeURI('https://s3.amazonaws.com/' + destination_bucket + '/' + path);
 
